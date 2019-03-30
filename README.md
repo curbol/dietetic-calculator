087c5918a0c8903afcbff0e2113ee184a005f5a0
curbol <cbollinger@321moonshot.com>
2019-03-30 16:21:00 -0500

    Experimenting with nuxt

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dietetic-calculator
+
+> Tool for common dietetic calculations
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3333
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Calculator',
+          to: '/'
+        },
+        {
+          icon: 'refresh',
+          title: 'Converter',
+          to: '/convert'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'About',
+          to: '/about'
+        }
+      ],
+      miniVariant: false,
+      title: 'Dietetic Calculator'
+    }
+  }
+}
+</script>
