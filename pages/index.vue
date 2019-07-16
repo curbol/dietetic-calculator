5499917061b49dc3b586c8633c31a5f04f991fda
Curtis Bollinger <cbollinger@321moonshot.com>
2019-07-16 15:54:41 -0500

    Experimenting with vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,101 +1,54 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex md12 lg6>
-      <material-card
-        color="cyan"
-        title="Employee Stats"
-        text="New employees on 15th September, 2016"
-      >
-        <v-data-table :headers="headers" :items="items" hide-actions>
-          <template slot="headerCell" slot-scope="{ header }">
-            <span
-              class="font-weight-light text-warning text--darken-3"
-              v-text="header.text"
-            />
-          </template>
-          <template slot="items" slot-scope="{ index, item }">
-            <td>{{ index + 1 }}</td>
-            <td>{{ item.name }}</td>
-            <td class="text-xs-right">{{ item.salary }}</td>
-            <td class="text-xs-right">{{ item.country }}</td>
-            <td class="text-xs-right">{{ item.city }}</td>
-          </template>
-        </v-data-table>
-      </material-card>
-    </v-flex>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container fill-height fluid grid-list-xl>
+    <v-layout wrap>
+      <v-flex md12 sm12 lg4>
+        <v-toolbar color="indigo" dark>
+          <v-toolbar-title>Calculators</v-toolbar-title>
+        </v-toolbar>
+        <v-card>
+          <v-container fluid grid-list-md>
+            <v-layout row wrap>
+              <v-flex xs8>
+                <span>test</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+      <v-flex md12 sm12 lg4>
+        <v-toolbar color="indigo" dark>
+          <v-toolbar-title>Calculators</v-toolbar-title>
+        </v-toolbar>
+        <v-card>
+          <v-container fluid grid-list-md>
+            <v-layout row wrap>
+              <v-flex xs8>
+                <span>test</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+      <v-flex md12 sm12 lg4>
+        <v-toolbar color="indigo" dark>
+          <v-toolbar-title>Calculators</v-toolbar-title>
+        </v-toolbar>
+        <v-card>
+          <v-container fluid grid-list-md>
+            <v-layout row wrap>
+              <v-flex xs8>
+                <span>test</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import Card from '~/components/material/Card.vue'
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-    materialCard: Card
-  },
   data() {
     return {
       headers: [
