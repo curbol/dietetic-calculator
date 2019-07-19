6e282714a8246a634d2a6e8e19c64e585e6e54b9
Curtis Bollinger <curtis.bollinger@gmail.com>
2019-07-19 16:12:32 -0500

    worked on calculators component.

--- components/Calculators.vue
+++ components/Calculators.vue
@@ -8,31 +8,41 @@
         <v-icon>check_box_outline_blank</v-icon>
       </v-btn>
     </template>
-    <v-list
-      v-for="(calcGroup, name, index) in calcGroups"
-      :key="index"
-      subheader
-      two-line
-    >
-      <v-divider v-if="index > 0"></v-divider>
-      <v-subheader>{{ name }}</v-subheader>
+    <v-list expand>
+      <v-list-group
+        v-for="calcGroup in calcGroups"
+        :key="calcGroup.type"
+        v-model="calcGroup.active"
+        :prepend-icon="calcGroup.icon"
+      >
+        <template v-slot:activator>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ calcGroup.type }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
 
-      <v-list-tile v-for="calc in calcGroup" :key="calc.id">
-        <v-list-tile-action>
-          <v-checkbox></v-checkbox>
-        </v-list-tile-action>
+        <v-list-tile
+          v-for="calc in calcGroup.calcs"
+          :key="calc.id"
+          @click="() => {}"
+        >
+          <v-list-tile-action>
+            <v-checkbox v-model="calc.active"></v-checkbox>
+          </v-list-tile-action>
 
-        <v-list-tile-content>
-          <v-list-tile-title>{{ calc.title }}</v-list-tile-title>
-          <v-list-tile-sub-title>{{ calc.subtitle }}</v-list-tile-sub-title>
-        </v-list-tile-content>
-      </v-list-tile>
+          <v-list-tile-content @click="calc.active = !calc.active">
+            <v-list-tile-title>{{ calc.title }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ calc.subtitle }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list-group>
     </v-list>
   </tool-card>
 </template>
 
 <script>
-import _ from 'lodash'
 import ToolCard from '@/components/ToolCard.vue'
 
 export default {
@@ -41,49 +51,58 @@ export default {
   },
   data() {
     return {
-      calculators: [
+      calcGroups: [
         {
-          id: 'bmi',
           type: 'Anthropometric',
-          title: 'Body Mass Index (BMI)',
-          subtitle: 'A measure of body fat in adults',
-          inputs: ['height', 'weight'],
-          selections: [],
-          outputUnit: 'kg/m²'
+          active: true,
+          icon: 'accessibility_new',
+          calcs: [
+            {
+              id: 'bmi',
+              active: false,
+              title: 'Body Mass Index (BMI)',
+              subtitle: 'A measure of body fat in adults',
+              inputs: ['height', 'weight'],
+              selections: [],
+              outputUnit: 'kg/m²'
+            },
+            {
+              id: 'ibw',
+              active: false,
+              title: 'Ideal Body Weight (IBW)',
+              subtitle: 'Estimated ideal weight for adults',
+              inputs: ['height'],
+              selections: ['gender'],
+              outputUnit: 'kg'
+            },
+            {
+              id: 'abw',
+              active: false,
+              title: 'Adjusted Body Weight (ABW)',
+              subtitle: 'Adjusted ideal weight for the obese',
+              inputs: ['height', 'weight'],
+              selections: ['gender'],
+              outputUnit: 'kg'
+            }
+          ]
         },
         {
-          id: 'ibw',
-          type: 'Anthropometric',
-          title: 'Ideal Body Weight (IBW)',
-          subtitle: 'Estimated ideal weight for adults',
-          inputs: ['height'],
-          selections: ['gender'],
-          outputUnit: 'kg'
-        },
-        {
-          id: 'abw',
-          type: 'Anthropometric',
-          title: 'Adjusted Body Weight (ABW)',
-          subtitle: 'Adjusted ideal weight for the obese',
-          inputs: ['height', 'weight'],
-          selections: ['gender'],
-          outputUnit: 'kg'
-        },
-        {
-          id: 'mifflin',
           type: 'Nutritional Needs',
-          title: 'Mifflin St. Jeor',
-          subtitle: 'Daily calorie needs for adults',
-          inputs: ['height', 'weight', 'age'],
-          selections: ['gender'],
-          outputUnit: 'kcal'
+          active: true,
+          icon: 'restaurant',
+          calcs: [
+            {
+              id: 'mifflin',
+              active: false,
+              title: 'Mifflin St. Jeor',
+              subtitle: 'Daily calorie needs for adults',
+              inputs: ['height', 'weight', 'age'],
+              selections: ['gender'],
+              outputUnit: 'kcal'
+            }
+          ]
         }
       ]
-    }
-  },
-  computed: {
-    calcGroups() {
-      return _.groupBy(this.calculators, 'type')
     }
   }
 }
