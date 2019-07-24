c0fab90a716fd54a7ada5b04f9b890b615dfd7db
Curtis Bollinger <curtis.bollinger@gmail.com>
2019-07-23 21:09:04 -0500

    Working on input

--- components/InputList.vue
+++ components/InputList.vue
@@ -18,41 +18,32 @@
         <v-icon>check_box_outline_blank</v-icon>
       </v-btn>
     </template>
-    <v-list expand>
-      <v-list-group
-        v-for="category in categories"
-        :key="category.id"
-        :value="category.active"
-        :prepend-icon="category.icon"
-        @click="toggleActivateCategory(category.id)"
-      >
-        <template v-slot:activator>
-          <v-list-tile>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ category.name }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-
-        <v-list-tile
-          v-for="calc in getCalcsByCategoryId(category.id)"
-          :key="calc.id"
-          @click="() => {}"
-        >
-          <v-list-tile-action>
-            <v-checkbox
-              :value="calc.selected"
-              @click.stop="toggleSelectCalculator(calc.id)"
-            ></v-checkbox>
-          </v-list-tile-action>
-
-          <v-list-tile-content @click="toggleSelectCalculator(calc.id)">
-            <v-list-tile-title>{{ calc.title }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ calc.subtitle }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list-group>
-    </v-list>
+    <v-container>
+      <v-layout column align-center>
+        <v-flex>
+          <v-text-field>
+            <v-icon slot="append" color="red">check_box</v-icon>
+            <v-icon slot="prepend" color="green">check_box</v-icon>
+          </v-text-field>
+        </v-flex>
+        <v-flex>
+          <v-text-field>
+            <v-icon slot="append" color="red">check_box</v-icon>
+            <v-icon slot="prepend" color="green">check_box</v-icon>
+          </v-text-field>
+        </v-flex>
+        <v-flex>
+          <v-layout>
+            <v-flex>
+              <v-text-field></v-text-field>
+            </v-flex>
+            <v-flex sm3 d-flex>
+              <v-select :items="items" label="kg"></v-select>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </tool-card>
 </template>
 
@@ -63,6 +54,11 @@ import ToolCard from '@/components/ToolCard.vue'
 export default {
   components: {
     ToolCard
+  },
+  data() {
+    return {
+      items: ['kg', 'lb']
+    }
   },
   computed: {
     ...mapState('calcs', ['categories', 'calculators']),
