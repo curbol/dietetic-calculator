c359c233f609ac55ae04c9ec71983e689ad179fe
Curtis Bollinger <cbollinger@321moonshot.com>
2019-09-20 09:45:50 -0500

    trying to get ava tests to work

--- test/ava.setup.js
+++ test/ava.setup.js
@@ -1,8 +1,8 @@
-import hooks from 'require-extension-hooks'
-import { config } from 'vue'
 require('browser-env')()
+const hooks = require('require-extension-hooks')
+const Vue = require('vue')
 
-config.productionTip = false
+Vue.config.productionTip = false
 
 // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
 window.Date = global.Date = Date
