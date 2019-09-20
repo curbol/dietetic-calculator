62c98ef5e0aefd3c97d01186befc76345e60465e
Curtis Bollinger <cbollinger@321moonshot.com>
2019-09-20 09:28:45 -0500

    updated to latest version of nuxt-create

--- functions/pipe.js
+++
@@ -1,3 +0,0 @@
-export default function pipe(...fns) {
-  return (x) => fns.reduce((v, f) => f(v), x)
-}
---
+++ test/specs/Converter.spec.js
@@ -0,0 +1,54 @@
+import { mount } from '@vue/test-utils'
+import test from 'ava'
+import Converter from '@/components/Converter.vue'
+
+test('is a Vue instance', (t) => {
+  const wrapper = mount(Converter)
+  t.is(wrapper.isVueInstance(), true)
+})
+
+// import { resolve } from 'path'
+// import { Nuxt, Builder } from 'nuxt'
+// import { JSDOM } from 'jsdom'
+// import test from 'ava'
+// import Vuetify from 'vuetify'
+
+// // We keep the nuxt and server instance
+// // So we can close them at the end of the test
+// let nuxt = null
+
+// // Init Nuxt.js and create a server listening on localhost:4000
+// test.before(async () => {
+//   const config = {
+//     dev: false,
+//     rootDir: resolve(__dirname, '..'),
+//     vuetify: new Vuetify()
+//   }
+//   nuxt = new Nuxt(config)
+//   await new Builder(nuxt).build()
+//   await nuxt.server.listen(3001, 'localhost')
+// }, 30000)
+
+// // Example of testing only generated html
+// test('Route / exits and render HTML', async (t) => {
+//   const context = {}
+//   const { html } = await nuxt.server.renderRoute('/', context)
+//   t.true(html.includes('<h1 class="red">Hello world!</h1>'))
+// })
+
+// // Example of testing via dom checking
+// test('Route / exits and render HTML with CSS applied', async (t) => {
+//   const context = {}
+//   const { html } = await nuxt.server.renderRoute('/', context)
+//   const { window } = new JSDOM(html).window
+//   const element = window.document.querySelector('.red')
+//   t.not(element, null)
+//   t.is(element.textContent, 'Hello world!')
+//   t.is(element.className, 'red')
+//   t.is(window.getComputedStyle(element).color, 'red')
+// })
+
+// // Close server and ask nuxt to stop listening to file changes
+// test.after('Closing server and nuxt.js', (t) => {
+//   nuxt.close()
+// })
