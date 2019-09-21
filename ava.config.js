3f43e70bc27eba06464d8715da7b9d6ba9ff70c4
Curtis Bollinger <curtis.bollinger@gmail.com>
2019-09-21 15:36:55 -0500

    trying to get tests to run.

--- ava.config.js
+++ ava.config.js
@@ -1,24 +1,9 @@
 export default {
-  require: ['./test/_setup.js'],
+  require: ['./test/ava.setup.js'],
   sources: ['**/*.{js,vue}'],
   files: ['test/specs/**/*'],
-  babel: {
-    testOptions: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            root: ['.'],
-            alias: {
-              '@': '.',
-              '~': '.'
-            }
-          }
-        ]
-      ]
-    }
-  },
   tap: true,
   serial: true,
-  verbose: true
+  verbose: true,
+  color: true
 }
