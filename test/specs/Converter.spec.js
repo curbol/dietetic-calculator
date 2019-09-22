e1d55067d4b14dd41d40196f0905317656f1b46d
Curtis Bollinger <curtis.bollinger@gmail.com>
2019-09-21 22:34:31 -0500

    made progress on getting tests to run.

--- test/specs/Converter.spec.js
+++ test/specs/Converter.spec.js
@@ -3,12 +3,25 @@ import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import test from 'ava'
 import Converter from '@/components/Converter.vue'
+import convertStore from '@/store/convert.js'
+import unitsStore from '@/store/units.js'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 localVue.use(Vuetify)
 
+let store
+
+test.beforeEach((t) => {
+  store = new Vuex.Store({
+    modules: {
+      convert: { ...convertStore, namespaced: true },
+      units: { ...unitsStore, namespaced: true }
+    }
+  })
+})
+
 test('component should mount', (t) => {
-  const wrapper = shallowMount(Converter, { localVue })
+  const wrapper = shallowMount(Converter, { localVue, store })
   t.is(wrapper.isVueInstance(), true)
 })
